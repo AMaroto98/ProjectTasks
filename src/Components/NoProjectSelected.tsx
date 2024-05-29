972ae48b15b60924bb18823e7d3d3ef69c947cfb
Antonio Maroto <antoniomarotoblasco@gmail.com>
2024-05-29 11:34:52 +0200

    Logo src fixed

--- src/Components/NoProjectSelected.tsx
+++ src/Components/NoProjectSelected.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export function NoProjectSelected() {
   return (
     <div className="no-project-selected">
-      <img src="src/assets/no-projects.png" alt="Logo" />
+      <img src="/logo.png" alt="Logo" />
       <h3>No Project Selected</h3>
       <p>Select a project or get started with a new one</p>
       <Link to={"/CreateProject"}>
